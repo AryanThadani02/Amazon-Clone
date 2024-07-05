19d68469e05f29bb1117f535909e508ccc74ebc8
Aryan Thadani <aryanthadani0@gmail.com>
2024-07-05 14:10:46 +0530

    updated signup and signin page

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,56 +1,43 @@
 /* eslint-disable no-unused-vars */
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { ArrowRight } from "lucide-react";
 import logo from "../assets/HomeCarousel/amazonin.svg";
 import { Link } from "react-router-dom";
 import { toast, Bounce } from 'react-toastify';
 import { myContext } from "./Context";
-import {
-  signInWithPopup,
-  GoogleAuthProvider,
-  createUserWithEmailAndPassword,
-} from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "./firebase";
 import { useNavigate } from "react-router-dom";
 
 export function SignUp() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [fullName, setFullName] = useState("");
   const navigate = useNavigate();
-  
-  const [userEmail,  setUseremail] = useContext(myContext);
-  
-  if(userEmail !== null){
+  const [userEmail, setUseremail] = useContext(myContext);
+
+  if (userEmail !== null) {
     navigate('/');
   }
 
-
-  const handleSignUpClick = () => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed up
-        const user = userCredential.user;
+  const handleGoogleSignUp = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
         console.log(user);
+        setUseremail(user.email);
         toast.success("Account Created Successfully!", {
           position: "bottom-right",
           theme: "colored",
-        })
-        navigate("/signin")
-        // ...
+        });
+        navigate("/");
       })
       .catch((error) => {
-        const errorCode = error.code;
         const errorMessage = error.message;
-        // ..
-
         toast.error(errorMessage, {
           position: "bottom-right",
           theme: "colored",
-        })
+        });
       });
   };
-
 
   return (
     <section>
@@ -78,78 +65,10 @@ export function SignUp() {
               Sign In
             </Link>
           </p>
-          <form action="#" method="POST" className="mt-8">
-            <div className="space-y-5">
-              <div>
-                <label
-                  htmlFor="name"
-                  className="text-base font-medium text-gray-900"
-                >
-                  {" "}
-                  Full Name{" "}
-                </label>
-                <div className="mt-2">
-                  <input
-                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                    type="text"
-                    placeholder="Full Name"
-                    id="name"
-                    onChange={(e) => setFullName(e.target.value)}
-                  ></input>
-                </div>
-              </div>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="text-base font-medium text-gray-900"
-                >
-                  {" "}
-                  Email address{" "}
-                </label>
-                <div className="mt-2">
-                  <input
-                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                    type="email"
-                    placeholder="Email"
-                    id="email"
-                    onChange={(e) => setEmail(e.target.value)}
-                  ></input>
-                </div>
-              </div>
-              <div>
-                <div className="flex items-center justify-between">
-                  <label
-                    htmlFor="password"
-                    className="text-base font-medium text-gray-900"
-                  >
-                    {" "}
-                    Password{" "}
-                  </label>
-                </div>
-                <div className="mt-2">
-                  <input
-                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                    type="password"
-                    placeholder="Password"
-                    id="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                  ></input>
-                </div>
-              </div>
-              <div>
-                <button
-                  type="button"
-                  onClick={handleSignUpClick}
-                  className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
-                >
-                  Create Account <ArrowRight className="ml-2" size={16} />
-                </button>
-              </div>
-            </div>
-          </form>
-          <div className="mt-3 space-y-3">
+          <div className="mt-8 space-y-5">
             <button
               type="button"
+              onClick={handleGoogleSignUp}
               className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
             >
               <span className="mr-2 inline-block">
@@ -163,22 +82,6 @@ export function SignUp() {
                 </svg>
               </span>
               Sign up with Google
-            </button>
-            <button
-              type="button"
-              className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
-            >
-              <span className="mr-2 inline-block">
-                <svg
-                  className="h-6 w-6 text-[#2563EB]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
-                </svg>
-              </span>
-              Sign up with Facebook
             </button>
           </div>
         </div>
